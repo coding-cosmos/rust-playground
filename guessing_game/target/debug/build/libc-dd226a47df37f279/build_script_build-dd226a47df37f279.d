156ab346278bc4d47c930cc4219b694356b6f203
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2026-03-09 15:38:17 +0530

    Add .gitignore

---
+++ guessing_game/target/debug/build/libc-dd226a47df37f279/build_script_build-dd226a47df37f279.d
@@ -0,0 +1,5 @@
+/home/sujal/Dev/rusty/rust-playground/guessing_game/target/debug/build/libc-dd226a47df37f279/build_script_build-dd226a47df37f279.d: /home/sujal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.178/build.rs
+
+/home/sujal/Dev/rusty/rust-playground/guessing_game/target/debug/build/libc-dd226a47df37f279/build_script_build-dd226a47df37f279: /home/sujal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.178/build.rs
+
+/home/sujal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.178/build.rs:
